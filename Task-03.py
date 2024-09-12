67cac7466d8ba76129d778a41f9318ba04a15acc
vishalhiwale <vishalhiwale31oct@gmail.com>
2024-09-12 17:57:31 +0530

    use downloaded bank.csv file to run program

--- Task-03.py
+++ Task-03.py
@@ -1,15 +1,16 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 
-url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank.zip'
-df = pd.read_csv(url, compression='zip', sep=';')
+url = "C:/Users/ASUS/Documents/GitHub/PRODIGY-INFOTECH-Task-03/bank.csv" # bank.csv file's path from your device
+data = pd.read_csv(url, sep=';')
 
-df_encoded = pd.get_dummies(df, drop_first=True)
-X = df_encoded.drop('y_yes', axis=1)  
-y = df_encoded['y_yes']
+data = pd.get_dummies(data, drop_first=True)
+
+X = data.drop('y_yes', axis=1)
+y = data['y_yes']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
@@ -18,9 +19,28 @@ clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 
-print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
-print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print(f'Accuracy: {accuracy_score(y_test, y_pred)}')
+print('Classification Report:')
+print(classification_report(y_test, y_pred))
+print('Confusion Matrix:')
+print(confusion_matrix(y_test, y_pred))
+
+param_grid = {
+    'max_depth': [3, 5, 10, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [2, 2, 4]
+}
+
+grid_search = GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid, cv=5, n_jobs=-1, verbose=1)
+grid_search.fit(X_train, y_train)
+
+print(f'Best Parameters: {grid_search.best_params_}')
+
+y_pred_optimized = grid_search.best_estimator_.predict(X_test)
+print(f'Optimized Accuracy: {accuracy_score(y_test, y_pred_optimized)}')
+print('Optimized Classification Report:')
+print(classification_report(y_test, y_pred_optimized))
 
 plt.figure(figsize=(20,10))
-plot_tree(clf, feature_names=X.columns, class_names=['No', 'Yes'], filled=True, rounded=True)
+plot_tree(clf, feature_names=X.columns, class_names=['No', 'Yes'], filled=True)
 plt.show()
